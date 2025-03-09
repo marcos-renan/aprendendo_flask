16d964743168b91bc217fe957f4a81b9490f6840
Marcos <devmarcos7@gmail.com>
2025-03-09 14:57:16 -0300

    criando segunda rota

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
-app = Flask()
+app = Flask(__name__)
 
 #Criando primeira rota
 @app.route("/")
 def home():
-    return "Hello World"
+    return "Hello World, I love Jesus!"
+
+@app.route("/login")
+def login():
+    return "Tela de login"
+
+if __name__ == '__main__' :
+    app.run(debug=True)
